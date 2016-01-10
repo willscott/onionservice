4a68a2624b9532d5219ae53a7950b30b049fa14b
Will Scott <willscott@gmail.com>
2016-01-10 22:56:52 +0100

    cleanup code

--- index.js
+++ index.js
@@ -38,6 +38,65 @@ var setKeyBlob = function (stream, blob) {
   stream.end(blob);
 };
 
+var onListening = function () {
+  this._onionInternalAddress = this.address();
+
+  this.address = function () {
+    return this._onionAddress || {};
+  };
+
+  this._onionAttach = function (key) {
+    var cmd = "ADD_ONION ";
+    if (key && key.length) {
+      cmd += key + " ";
+    } else {
+      cmd += "NEW:BEST ";
+    }
+    if (this._onionOpts.cacheKeyMaterial === false) {
+      cmd += "Flags=DiscardPK ";
+    }
+    cmd += "Port=" + this._onionPort + "," + this._onionInternalAddress.port + "\r\n";
+    this._onionSocket.write(cmd);
+  }.bind(this);
+
+  connecttor.connect(function (controlSocket) {
+    if (!controlSocket) {
+      this.emit('error', new Error("Onion Connection failed."));
+      this.close();
+    }
+
+    this._onionSocket = controlSocket;
+    // Tor will associate the onion with our control socket that asked for it.
+    // Closing at the same time as the server means they clean up together.
+    this._onionSocket.on('close', this.close.bind(this));
+    this.on('close', this._onionSocket.end.bind(this._onionSocket));
+
+    this._onionSocket.pipe(split()).on("data", function (line) {
+      if (line.indexOf("250-ServiceID=") === 0) {
+        this._onionAddress = {
+          family: 'Onion',
+          address: line.substr(14) + ".onion",
+          port: this._onionPort
+        };
+      } else if (line.indexOf("250-PrivateKey=") === 0) {
+        var keyBlob = line.substr(15).trim();
+        setKeyBlob(this._onionOpts.keyMaterial, keyBlob);
+      } else if (line.indexOf("250 OK") === 0) {
+        this.emit('listening');
+      } else {
+        // Unexpected condition. raise.
+        this.emit('error', new Error(line));
+      }
+    }.bind(this));
+
+    if (this._onionOpts.keyMaterial) {
+      getKeyBlob(this._onionOpts.keyMaterial, this._onionAttach.bind(this));
+    } else {
+      this._onionAttach();
+    }
+  }.bind(this));
+};
+
 var listen = function(httpListen, options, port) {
   this._onionPort = port;
   this._onionOpts = options || {};
@@ -45,65 +104,19 @@ var listen = function(httpListen, options, port) {
     this._onionOpts.keyMaterial = path.join(process.cwd(), ".onionservice");
   }
 
+  // capture first 'listening' event to instead cue starting the onion.
+  var realEmit = this.emit;
+  this.emit = function() {
+    if (arguments[0] === 'listening') {
+      onListening.call(this);
+      this.emit = realEmit;
+    } else {
+      realEmit.apply(this, arguments);
+    }
+  }.bind(this);
+
   // Assign arbitrary high port on localhost for the http server.
-  httpListen.call(this, 0, '127.0.0.1', function () {
-    this._onionInternalAddress = this.address();
-
-    this.address = function () {
-      return this._onionAddress || {};
-    };
-
-    this._onionAttach = function (key) {
-      var cmd = "ADD_ONION ";
-      if (key && key.length) {
-        cmd += key + " ";
-      } else {
-        cmd += "NEW:BEST ";
-      }
-      if (this._onionOpts.cacheKeyMaterial === false) {
-        cmd += "Flags=DiscardPK ";
-      }
-      cmd += "Port=" + this._onionPort + "," + this._onionInternalAddress.port + "\r\n";
-      controlSocket.write(cmd);
-    };
-
-    connecttor.connect(function (controlSocket) {
-      if (!controlSocket) {
-        this.emit('error', new Error("Onion Connection failed."));
-        this.close();
-      }
-
-      this._onionSocket = controlSocket;
-      // Tor will associate the onion with our control socket that asked for it.
-      // Closing at the same time as the server means they clean up together.
-      this._onionSocket.on('close', this.close.bind(this));
-      this.on('close', this._onionSocket.close.bind(this._onionSocket));
-
-      this._onionSocket.pipe(split()).on("data", function (line) {
-        if (line.indexOf("250-ServiceID=") === 0) {
-          this._onionAddress = {
-            family: 'Onion',
-            address: line.substr(14) + ".onion",
-            port: this._onionPort
-          };
-        } else if (line.indexOf("250-PrivateKey=") === 0) {
-          var keyBlob = line.substr(15).trim();
-          setKeyBlob(this._onionOpts.keyMaterial, keyBlob);
-        } else if (line.indexOf("250 OK") === 0) {
-          this.emit('listening');
-        } else {
-          // Unexpected condition. raise.
-          this.emit('error', new Error(line));
-        }
-      }.bind(this));
-
-      if (this._onionOpts.keyMaterial) {
-        getKeyBlob(this._onionOpts.keyMaterial, this._onionAttach.bind(this));
-      } else {
-        this._onionAttach();
-      }
-    }.bind(this));
-  }.bind(this));
+  httpListen.call(this, 0, '127.0.0.1');
 };
 
 var createServer = function (options, requestListener) {
