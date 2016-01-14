a6034e30b056b7e0fb6e3fe32545f6a2625eb589
Will Scott <willscott@gmail.com>
2016-01-13 21:11:38 -0800

    attempts at interacting with system tor

--- index.js
+++ index.js
@@ -47,6 +47,51 @@ var onListening = function () {
     return this._onionAddress || {};
   };
 
+  // For older 0.2.6 tor.
+  this._setconfAttach = function (key) {
+    var hsdir = path.join(process.cwd(), ".tor.hs");
+    if (!fs.existsSync(hsdir)) {
+      fs.mkdirSync(hsdir, '770');
+      // Set permissions.
+      if (process.platform === 'linux') {
+        var posix = require('posix');
+        var knownTorGroups = ['debian-tor', 'toranon'];
+        for (var i = 0; i < knownTorGroups.length; i += 1) {
+          try {
+            var torid = posix.getgrnam(knownTorGroups[i]);
+            var stats = fs.statSync(hsdir);
+            fs.chownSync(hsdir, stats.uid, torid.gid);
+            break;
+          } catch (e) {
+            continue;
+          }
+        }
+      }
+    } else if (this._onionOpts.cacheKeyMaterial === false) {
+      fs.unlinkSync(path.join(process.cwd(), ".tor.hs", "private_key"));
+    }
+    if (key && key.length) {
+      var data = JSON.parse(key);
+      fs.writeFileSync(path.join(process.cwd(), ".tor.hs", "hostname"), data.name);
+      fs.writeFileSync(path.join(process.cwd(), ".tor.hs", "private_key"), data.pk);
+    }
+    // Tor will write 'hostname' and 'private_key' files into this dir.
+    var cmd = "SETCONF HiddenServiceDir " + path.join(process.cwd(), ".tor.hs") + "\n" +
+        "SETCONF HiddenServicePort " + this._onionPort +
+        " 127.0.0.1:" + this._onionInternalAddress.port + "\n";
+    this._onionSocket.write(cmd);
+  }.bind(this);
+
+  this._getConfInfo = function () {
+    var data = {};
+    data.host = fs.readFileSync(path.join(process.cwd(), ".tor.hs", "hostname"));
+    if (this._onionOpts.cacheKeyMaterial !== false) {
+      data.pk = fs.readFileSync(path.join(process.cwd(), ".tor.hs", "private_key"));
+    }
+    return data;
+  };
+
+  // For newer 0.2.7 tor.
   this._onionAttach = function (key) {
     var cmd = "ADD_ONION ";
     if (key && key.length) {
@@ -74,7 +119,22 @@ var onListening = function () {
     this.on('close', this._onionSocket.end.bind(this._onionSocket));
 
     this._onionSocket.pipe(split()).on("data", function (line) {
-      if (line.indexOf("250-ServiceID=") === 0) {
+      if (line.indexOf("250-version=") === 0) {
+        // Response to "GETINFO version" query.
+        var attach;
+        if (line.indexOf("0.2.6") > 0) {
+          this._onionVersion = "26";
+          attach = this._setconfAttach;
+        } else {
+          this._onionVersion = "27";
+          attach = this._onionAttach;
+        }
+        if (this._onionOpts.keyMaterial) {
+          getKeyBlob(this._onionOpts.keyMaterial, attach.bind(this));
+        } else {
+          attach();
+        }
+      } else if (line.indexOf("250-ServiceID=") === 0) {
         this._onionAddress = {
           family: 'Onion',
           address: line.substr(14) + ".onion",
@@ -84,6 +144,15 @@ var onListening = function () {
         var keyBlob = line.substr(15).trim();
         setKeyBlob(this._onionOpts.keyMaterial, keyBlob);
       } else if (line.indexOf("250 OK") === 0) {
+        if (this._onionVersion === "26") {
+          var info = this._getConfInfo();
+          this._onionAddress = {
+            family: 'Onion',
+            address: info.host,
+            port: this._onionPort
+          };
+          setKeyBlob(this._onionOpts.keyMaterial, JSON.serialize(info));
+        }
         this.emit('listening');
       } else {
         // Unexpected condition. raise.
@@ -91,11 +160,7 @@ var onListening = function () {
       }
     }.bind(this));
 
-    if (this._onionOpts.keyMaterial) {
-      getKeyBlob(this._onionOpts.keyMaterial, this._onionAttach.bind(this));
-    } else {
-      this._onionAttach();
-    }
+    this._onionSocket.write("GETINFO version\n");
   }.bind(this));
 };
 
